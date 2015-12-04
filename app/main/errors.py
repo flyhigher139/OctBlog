0dab6435301a026ff1b559a03fe7b3fed463adfa
gevin <flyhigher139@gmail.com>
2015-12-04 09:58:37 +0800

    optimize unmatchable

--- app/main/errors.py
+++ app/main/errors.py
@@ -3,7 +3,9 @@ from flask import render_template
 from .views import get_base_data
 
 def handle_unmatchable(*args, **kwargs):
-    return 'unmatchable page', 404
+    # return 'unmatchable page', 404
+    data = get_base_data()
+    return render_template('main/404.html', **data), 404
 
 def page_not_found(e):
     data = get_base_data()
