1ffc78c64123b81e8da6b47bcda61f319c46ebf9
gevin <flyhigher139@gmail.com>
2017-06-23 14:27:55 +0800

    frontend fix time display

--- app/OctBlog/__init__.py
+++ app/OctBlog/__init__.py
@@ -7,17 +7,19 @@ from flask import Flask
 from flask_mongoengine import MongoEngine
 from flask_login import LoginManager
 from flask_principal import Principal 
+from flask_moment import Moment
 
 from .config import config
 
 db = MongoEngine()
 
 login_manager = LoginManager()
-# login_manager.session_protection = 'strong'
 login_manager.session_protection = 'basic'
 login_manager.login_view = 'accounts.login'
 
 principals = Principal()
+
+moment = Moment()
 
 def create_app(config_name):
     app = Flask(__name__, 
@@ -29,6 +31,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     principals.init_app(app)
+    moment.init_app(app)
 
     from main.urls import main as main_blueprint, blog_admin as blog_admin_blueprint
     from accounts.urls import accounts as accounts_blueprint
