ac62c9f02ee17d773323665495641d2ddcc67b0a
renmu <1101022351@qq.com>
2019-07-18 17:22:45 +0800

    add mongodb authentication

--- app/OctBlog/config.py
+++ app/OctBlog/config.py
@@ -3,11 +3,13 @@
 from __future__ import unicode_literals
 import os, sys, datetime
 
+
 def get_env_value(key, default_value=''):
     if sys.version_info < (3, 0):
         return os.environ.get(key, default_value).decode('utf8')
     else:
         return os.environ.get(key, default_value)
+
 
 # if sys.version_info < (3, 0):
 #     OctBlogSettings = {
@@ -136,7 +138,7 @@ def get_env_value(key, default_value=''):
 
 
 OctBlogSettings = {
-    'post_types': ('post', 'page'), # deprecated
+    'post_types': ('post', 'page'),  # deprecated
     'allow_registration': os.environ.get('allow_registration', 'false').lower() == 'true',
     'allow_su_creation': os.environ.get('allow_su_creation', 'false').lower() == 'true',
     'allow_donate': os.environ.get('allow_donate', 'true').lower() == 'true',
@@ -153,31 +155,34 @@ OctBlogSettings = {
         'baidu_site_verification': os.environ.get('baidu_site_verification') or '87654321',
         'sogou_site_verification': os.environ.get('sogou_site_verification') or '87654321',
     },
-    'search_engine_submit_urls':{
+    'search_engine_submit_urls': {
         'baidu': os.environ.get('baidu_submit_url')
     },
-    'pagination':{
+    'pagination': {
         'per_page': int(os.environ.get('per_page', 5)),
         'admin_per_page': int(os.environ.get('admin_per_page', 10)),
         'archive_per_page': int(os.environ.get('admin_per_page', 20)),
     },
-    'blog_comment':{
+    'blog_comment': {
         'allow_comment': os.environ.get('allow_comment', 'true').lower() == 'true',
-        'comment_type': os.environ.get('comment_type', 'octblog').lower(), # currently, OctBlog only supports duoshuo comment
-        'comment_opt':{
-            'octblog': 'oct-blog', # shotname of octblog
-            'duoshuo': 'oct-blog', # shotname of duoshuo
-            }
+        'comment_type': os.environ.get('comment_type', 'octblog').lower(),
+    # currently, OctBlog only supports duoshuo comment
+        'comment_opt': {
+            'octblog': 'oct-blog',  # shotname of octblog
+            'duoshuo': 'oct-blog',  # shotname of duoshuo
+        }
     },
     'donation': {
         'allow_donate': os.environ.get('allow_donate', 'true').lower() == 'true',
         'donation_msg': get_env_value('donation_msg', 'You can donate to me if the article makes sense to you'),
-        'donation_img_url': os.environ.get('donation_img_url') or 'http://img-own.igevin.info/weixin-pay.jpg?imageView/2/w/300'
+        'donation_img_url': os.environ.get(
+            'donation_img_url') or 'http://img-own.igevin.info/weixin-pay.jpg?imageView/2/w/300'
     },
     'wechat': {
         'display_wechat': os.environ.get('display_wechat', 'true').lower() == 'true',
         'wechat_msg': get_env_value('wechat_msg', 'Welcome to follow my wechat'),
-        'wechat_image_url': os.environ.get('wechat_image_url') or 'http://7tsygu.com1.z0.glb.clouddn.com/gevin-view.jpg?imageView/2/w/150',
+        'wechat_image_url': os.environ.get(
+            'wechat_image_url') or 'http://7tsygu.com1.z0.glb.clouddn.com/gevin-view.jpg?imageView/2/w/150',
         'wechat_title': get_env_value('wechat_title', 'GevinView'),
     },
     'copyright': {
@@ -187,19 +192,22 @@ OctBlogSettings = {
     'only_abstract_in_feed': os.environ.get('only_abstract_in_feed', 'false').lower() == 'true',
     'allow_share_article': os.environ.get('allow_share_article', 'true').lower() == 'true',
     'gavatar_cdn_base': os.environ.get('gavatar_cdn_base', '//cdn.v2ex.com/gravatar/'),
-    'gavatar_default_image': os.environ.get('gavatar_default_image', 'http://7tsygu.com1.z0.glb.clouddn.com/user-avatar.jpg'),
+    'gavatar_default_image': os.environ.get('gavatar_default_image',
+                                            'http://7tsygu.com1.z0.glb.clouddn.com/user-avatar.jpg'),
     'background_image': {
         'home': os.environ.get('bg_home') or 'http://7d9q7a.com1.z0.glb.clouddn.com/octblog-bg.jpg',
         'post': os.environ.get('bg_post') or 'http://7d9q7a.com1.z0.glb.clouddn.com/octblog-bg.jpg',
         'about': os.environ.get('bg_about') or 'http://7d9q7a.com1.z0.glb.clouddn.com/octblog_about.jpg',
         'qiniu': os.environ.get('qiniu') or 'http://assets.qiniu.com/qiniu-transparent.png',
     },
-    'daovoice':{
-        'allow_daovoice': (os.environ.get('allow_daovoice', 'false').lower() == 'true' and os.environ.get('daovoice_app_id') is not None),
+    'daovoice': {
+        'allow_daovoice': (os.environ.get('allow_daovoice', 'false').lower() == 'true' and os.environ.get(
+            'daovoice_app_id') is not None),
         'app_id': os.environ.get('daovoice_app_id'),
     }
 
 }
+
 
 class Config(object):
     DEBUG = False
@@ -219,32 +227,44 @@ class Config(object):
 
     REMEMBER_COOKIE_DURATION = datetime.timedelta(hours=3)
 
-
     @staticmethod
     def init_app(app):
         pass
 
+
 class DevConfig(Config):
     DEBUG = True
+    MONGODB_SETTINGS = {
+        'db': os.environ.get('DB_NAME') or 'OctBlog',
+        'host': os.environ.get('MONGO_HOST') or 'localhost',
+        # 'username': 'username',
+        # 'password': 'password',
+        # 'port': 12345
+    }
+
 
 class PrdConfig(Config):
     # DEBUG = False
     DEBUG = os.environ.get('DEBUG', 'false').lower() == 'true'
     MONGODB_SETTINGS = {
-            'db': os.environ.get('DB_NAME') or 'OctBlog',
-            'host': os.environ.get('MONGO_HOST') or 'localhost',
-            # 'port': 12345
-        }
+        'db': os.environ.get('DB_NAME') or 'OctBlog',
+        'host': os.environ.get('MONGO_HOST') or 'localhost',
+        # 'username': 'username',
+        # 'password': 'password',
+        # 'port': 12345
+    }
+
 
 class TestingConfig(Config):
     TESTING = True
     DEBUG = True
-    
+
     WTF_CSRF_ENABLED = False
     MONGODB_SETTINGS = {
         'db': 'OctBlogTest',
-        'is_mock':True
-        }
+        'is_mock': True
+    }
+
 
 config = {
     'dev': DevConfig,
